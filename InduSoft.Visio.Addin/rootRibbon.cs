0d10b2a581aa6ac6efec93ffc28286d7ae9949a4
g10101k <g101k@mail.ru>
2016-12-29 12:30:19 +0300

    Add ribbon

---
+++ InduSoft.Visio.Addin/rootRibbon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace InduSoft.Visio.Addin
+{
+    public partial class rootRibbon
+    {
+        public event Action btnTestClicked;
+        private void InduSoft_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (btnTestClicked != null)
+                btnTestClicked();
+        }
+    }
+}
